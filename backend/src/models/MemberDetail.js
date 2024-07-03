c3227edcfc385618beaa56549b2e814d6f34ec1f
rmxburhan <rizalburhanudin556@gmail.com>
2024-07-03 15:59:48 +0700

    FIX : add reference for memberId in Member model

--- backend/src/models/MemberDetail.js
+++ backend/src/models/MemberDetail.js
@@ -3,7 +3,8 @@ const {model, Schema, SchemaType} = required("mongoose");
 const memberDetailSchema = new Schema({
     memberId : {
         type : Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref : "User"
     },
     weight : {
         type : Number,
