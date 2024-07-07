a6ed50155b10772b3966cd92abde5c38a79de168
rmxburhan <rizalburhanudin556@gmail.com>
2024-07-07 19:08:25 +0700

    UPDATE : --

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-const notFoundHandler = require("./middleware/notFoundHandler");
-const connectMonggose = require("./utils/db");
-const routeIndex = require("./routes/routesIndex");
-const morgan = require("morgan");
+const notFoundHandler = require('./middleware/notFoundHandler');
+const connectMonggose = require('./utils/db');
+const routeIndex = require('./routes/routesIndex');
+const morgan = require('morgan');
+const cors = require('cors');
 // const chalk = require("chalk");
 
 dotenv.config();
 
-app.use(express.static("./public"))
-
+app.use(express.static('./public'));
+app.use(cors({ credentials: true }));
 // morgan.token("statusColor", (req, res) => {
 //   const status = res.statusCode;
 //   if (status >= 500) return chalk.red(status);
@@ -23,17 +24,17 @@ app.use(express.static("./public"))
 // app.use(
 //   morgan(":method :url :statusColor :response-time ms - :res[content-length]")
 // );
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use("/api", routeIndex);
+app.use('/api', routeIndex);
 
 app.use(notFoundHandler);
 
 connectMonggose();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+    console.log(`Server berjalan di http://localhost:${port}`);
 });
