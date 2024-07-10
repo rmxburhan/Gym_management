3e8d2f083d795cd070a0305dd7c0d97db809f3b4
rmxburhan <rizalburhanudin556@gmail.com>
2024-07-10 18:22:32 +0700

    FIX : apply deletedAt when find user data

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,96 +1,138 @@
-const User = require("../models/User");
-const {body} = require("express-validator")
-const jwt = require("jsonwebtoken")
+const User = require('../models/User');
+const { body } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
-const loginValidationRules = ()=> {
-return [
-    body("email").exists().withMessage("email cannot be empty").isString().withMessage("email must be a string").isEmail().withMessage("email is invalid").normalizeEmail(),
-    body("password").exists().withMessage("password cannot be empty").isString().withMessage("password must be a string").isLength({min : 6}).withMessage("password minimal must contain 6 characters").trim()
-]
-}
+const loginValidationRules = () => {
+    return [
+        body('email')
+            .exists()
+            .withMessage('email cannot be empty')
+            .isString()
+            .withMessage('email must be a string')
+            .isEmail()
+            .withMessage('email is invalid')
+            .normalizeEmail(),
+        body('password')
+            .exists()
+            .withMessage('password cannot be empty')
+            .isString()
+            .withMessage('password must be a string')
+            .isLength({ min: 6 })
+            .withMessage('password minimal must contain 6 characters')
+            .trim(),
+    ];
+};
 
-const loginHandler = async (req, res)  => {
+const loginHandler = async (req, res) => {
     try {
-        const {
-            email,
-            password
-        } = req.body;
+        const { email, password } = req.body;
 
-        const user = await User.findOne({email : email});
-        
+        const user = await User.findOne({ email: email, deletedAt: undefined });
+
         if (!user) {
             return res.status(404).json({
                 success: false,
-                message : "Email is not registered!"
-            })
+                message: 'Email is not registered!',
+            });
         }
-          
 
         var isMatch = user.comparePassword(password);
 
         if (!isMatch) {
             return res.status(400).json({
-                success : false, 
-                message : "Password is incorrect!"
-            })
+                success: false,
+                message: 'Password is incorrect!',
+            });
         }
         let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
-            expiresIn : '1h'
+            expiresIn: '1h',
         });
         return res.status(200).json({
-            success : true,
-            message : "Login successfull",
-            data : {
+            success: true,
+            message: 'Login successfull',
+            data: {
                 token,
-                user
-            }
-        })
+                user,
+            },
+        });
     } catch (error) {
         return res.status(500).json({
             success: false,
-            errors : error
-        })
+            errors: error,
+        });
     }
-    const {username, password} = req.body; 
-}
+    const { username, password } = req.body;
+};
 
 const registerValidationRules = () => {
     return [
-        body("name").exists().withMessage("name cannot be empty").isString().withMessage("name must be a string").trim().escape(),
+        body('name')
+            .exists()
+            .withMessage('name cannot be empty')
+            .isString()
+            .withMessage('name must be a string')
+            .trim()
+            .escape(),
 
-        body("email").exists().withMessage("email cannot be empty").isEmail().withMessage("please input valid email").normalizeEmail(),
-        
-        body("password").exists().withMessage("password cannot be empty").isString().withMessage("password must be a string").isLength({min : 6}).withMessage("password must be contain 6 characters").trim(),
+        body('email')
+            .exists()
+            .withMessage('email cannot be empty')
+            .isEmail()
+            .withMessage('please input valid email')
+            .normalizeEmail(),
 
-        body("dateOfBirth").exists().withMessage("date of birth cannot be empty").isDate().withMessage("date is invalid!").trim(),
+        body('password')
+            .exists()
+            .withMessage('password cannot be empty')
+            .isString()
+            .withMessage('password must be a string')
+            .isLength({ min: 6 })
+            .withMessage('password must be contain 6 characters')
+            .trim(),
 
-        body("gender").exists().withMessage("gender cannot be empty").isString().withMessage("gender must be a string").isIn(['male', 'female']).withMessage("invalid gender input, valid option male | female").trim(),
+        body('dateOfBirth')
+            .exists()
+            .withMessage('date of birth cannot be empty')
+            .isDate()
+            .withMessage('date is invalid!')
+            .trim(),
 
-        body("address").exists().withMessage("addresss cannot be empty").isString().withMessage("address must be a string").trim(),
-    ]
-}
+        body('gender')
+            .exists()
+            .withMessage('gender cannot be empty')
+            .isString()
+            .withMessage('gender must be a string')
+            .isIn(['male', 'female'])
+            .withMessage('invalid gender input, valid option male | female')
+            .trim(),
 
-const registerHandler = async (req,res )=> {
+        body('address')
+            .exists()
+            .withMessage('addresss cannot be empty')
+            .isString()
+            .withMessage('address must be a string')
+            .trim(),
+    ];
+};
+
+const registerHandler = async (req, res) => {
     try {
-        const {
-            name, 
-            email,
-            password,
-            dateOfBirth,
-            gender,
-            address
-        } = req.body
-    
-        const isExist = await User.findOne({email : email});
+        const { name, email, password, dateOfBirth, gender, image, address } =
+            req.body;
+
+        const isExist = await User.findOne({
+            email: email,
+            deletedAt: undefined,
+        });
 
         if (isExist) {
             console.log(isExist);
             return res.status(400).json({
-                success :false,
-                message : "email already registered!"
-            })
+                success: false,
+                message: 'email already registered!',
+            });
         }
-    
+
         const user = new User({
             name,
             email,
@@ -98,37 +140,38 @@ const registerHandler = async (req,res )=> {
             password,
             dateOfBirth,
             gender,
+            role: 'member',
         });
-    
+
         const isSuccess = await user.save();
-    
+
         if (!isSuccess) {
             return res.status(500).json({
-                success : false,
-                message : "failed to add user data"
-            })
+                success: false,
+                message: 'failed to add user data',
+            });
         }
 
         return res.status(201).json({
-            success : true,
-            message : "Register success!",
-            data : {
-                user
-            }
-        })
+            success: true,
+            message: 'Register success!',
+            data: {
+                user,
+            },
+        });
     } catch (error) {
         if (error) {
             return res.status(500).json({
-                success : false,
-                error
-            })
+                success: false,
+                error,
+            });
         }
     }
-}
+};
 
-module.exports =  {
+module.exports = {
     registerHandler,
     registerValidationRules,
     loginHandler,
-    loginValidationRules
-}
+    loginValidationRules,
+};
