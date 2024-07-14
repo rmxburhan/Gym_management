a8badaa913e33ef1e9b6a6de92c42bec773ac114
rmxburhan <rizalburhanudin556@gmail.com>
2024-07-14 20:32:36 +0700

    CHANGE : change authorization middleware

--- backend/src/middleware/authorizationMiddleware.js
+++ backend/src/middleware/authorizationMiddleware.js
@@ -1,41 +1,55 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const authorize = async (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ');
+const authorize = (roles) => {
+    return async (req, res, next) => {
+        try {
+            const token = req.headers.authorization?.split(' ');
 
-        if (token[0] !== 'Bearer') {
-            return res.status().json({
-                message: 'Token invalid',
+            if (token[0] !== 'Bearer') {
+                return res.status().json({
+                    message: 'Token invalid',
+                });
+            }
+
+            const decoded = jwt.verify(token[1], process.env.JWT_SECRET);
+
+            if (!decoded) {
+                return res.status(401).json({
+                    message: 'Token invalid',
+                });
+            }
+
+            const user = await User.findOne({
+                _id: decoded._id,
+                deletedAt: undefined,
+            });
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Unauthorized',
+                });
+            }
+
+            req.user = user;
+
+            let allowed = false;
+            roles.forEach((element) => {
+                console.log(element);
+                if (element == user.role) {
+                    allowed = true;
+                }
+            });
+            if (!allowed) {
+                return res.status(403).json({
+                    message: 'Forbidden',
+                });
+            }
+            next();
+        } catch (error) {
+            return res.status(400).json({
+                message: 'Token is invalid',
             });
         }
-
-        const decoded = jwt.verify(token[1], process.env.JWT_SECRET);
-
-        if (!decoded) {
-            return res.status(401).json({
-                message: 'Token invalid',
-            });
-        }
-
-        const user = await User.findOne({
-            _id: decoded._id,
-            deletedAt: undefined,
-        });
-        if (!user) {
-            return res.status(401).json({
-                message: 'Unauthorized',
-            });
-        }
-
-        req.user = user;
-
-        next();
-    } catch (error) {
-        return res.status(400).json({
-            message: 'Token is invalid',
-        });
-    }
+    };
 };
 
 module.exports = authorize;
--- backend/src/routes/routesIndex.js
+++ backend/src/routes/routesIndex.js
@@ -9,20 +9,13 @@ const authorize = require('../middleware/authorizationMiddleware');
 const adminSeed = require('../seef/seedDatabase');
 const membershipRoute = require('./membershipIndex');
 
-const {
-    adminAuthorize,
-    staffAuthorize,
-    trainerAuthorize,
-    memberAuthorize,
-} = require('../middleware/authorization');
-
 route.use('/auth', authRoute);
-route.use('/profile', authorize, profileRoute);
-route.use('/users', authorize, adminAuthorize, userRoute);
-route.use('/attendances', authorize, attendanceRoute);
-route.use('/classes', authorize, classRoute);
-route.use('/employees', authorize, adminAuthorize, employeeRoute);
+route.use('/profile', authorize(['admin', 'member', 'employee']), profileRoute);
+route.use('/users', authorize(['admin']), userRoute);
+route.use('/attendances', authorize(['admin']), attendanceRoute);
+route.use('/classes', authorize(['admin']), classRoute);
+route.use('/employees', authorize(['admin']), employeeRoute);
 route.get('/seedAdmin', adminSeed);
-route.use('/memberships', authorize, adminAuthorize, membershipRoute);
+route.use('/memberships', authorize(['admmin']), membershipRoute);
 
 module.exports = route;
