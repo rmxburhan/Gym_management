6cf6e4d8f7276589e8fb6c7511eda42ac5440a72
Rizal Burhanudin <rizalburhanudin556@gmail.com>
2024-07-30 08:49:36 +0700

    update : static folder and fix : get trainers

--- backend/src/startup/index.ts
+++ backend/src/startup/index.ts
@@ -1,10 +1,12 @@
 import cors from "cors";
 import express, { Express } from "express";
 import morgan from "morgan";
+import path from "path";
 
 export default (app: Express) => {
   app.use(express.static("public"));
   app.use(cors({ credentials: true }));
+  app.use(express.static(path.join(process.cwd(), "/public")));
 
   app.use(morgan("combined"));
   app.use(express.json());
